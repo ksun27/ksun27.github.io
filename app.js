e07bde685acd8f284b9e74d139dd1474e430a2ed
ksun27 <ksun2715@gmail.com>
2020-06-10 05:34:23 -0400

    update :3

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-
-
+// TOGGLE HEADER-NAV
 // Show dropdown when clicked
-$('.nav-btn').on('click', function(e) {
-  $('#header-menu').toggleClass('active');
-  $('.nav-btn').toggleClass('active');
-  $('#header').toggleClass('translucent');
-  $('.dropdown-menu').toggleClass('active')
+$('#header-btn').on('click', function(e) {
+    $('#header-menu').toggleClass('active');
+    $('.nav-btn').toggleClass('active');
 });
 
+// Hide menu after clicking menu item
+$('.dropdown-menu li').on('click', function(e) {
+    $('#header-menu').removeClass('active');
+    $('.nav-btn').removeClass('active');
+});
 
+$(document).ready(function() {
 
-$(document).ready(function () {
-
-  if ($(window).scrollTop() >= 200) {
-    $('#header').addClass('header-dark');
-    $('#header').removeClass('header-clear-mod');
-  } else {
-    $('#header').addClass('header-clear-mod');
-    $('#header').removeClass('header-dark');
-  }
-
-  $(window).scroll(function () {
     if ($(window).scrollTop() >= 200) {
-      $('#header').addClass('header-dark');
-      $('#header').removeClass('header-clear-mod');
-      $('#header').removeClass('header-clear');
-      $('#fade').addClass('faded');
+        $('#header').addClass('header-dark');
+        $('#header').removeClass('header-clear-mod');
     } else {
-      $('#header').addClass('header-clear-mod');
-      $('#header').addClass('header-clear');
-      $('#header').removeClass('header-dark');
-      $('#fade').removeClass('faded');
+        $('#header').addClass('header-clear-mod');
+        $('#header').removeClass('header-dark');
     }
-  });
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() >= 200) {
+            $('#header').addClass('header-dark');
+            $('#header').removeClass('header-clear-mod');
+            $('#header').removeClass('header-clear');
+            $('#fade').addClass('faded');
+        } else {
+            $('#header').addClass('header-clear-mod');
+            $('#header').addClass('header-clear');
+            $('#header').removeClass('header-dark');
+            $('#fade').removeClass('faded');
+        }
+    });
 });
